0d94882aaa50d075a2b541b92dc539cec7fd428c
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-07-11 19:24:51 -0400

    add proyecto base GestionEvento

---
+++ src/main/java/cl/td/g2/eventos/G2GestionEventosApplication.java
@@ -0,0 +1,13 @@
+package cl.td.g2.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2GestionEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2GestionEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/td/g2/eventos/G2GestionEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.td.g2.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2GestionEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
