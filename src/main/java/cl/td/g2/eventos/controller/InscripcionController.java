f1297f348b60fdc9005c9c965bd7b709ee392496
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-08-01 00:51:43 -0400

    Desarrollar InscripcionController para exponer endpoints de API REST para la gestión de Inscripcion

--- src/main/java/cl/td/g2/eventos/controller/InscripcionController.java
+++ src/main/java/cl/td/g2/eventos/controller/InscripcionController.java
@@ -1,12 +1,14 @@
 package cl.td.g2.eventos.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import cl.td.g2.eventos.dto.InscripcionDTO;
 import cl.td.g2.eventos.service.InscripcionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/inscripciones")
@@ -16,26 +18,27 @@ public class InscripcionController {
     private InscripcionService inscripcionService;
 
     @GetMapping
-    public List<InscripcionDTO> getAllInscripciones() {
-        return inscripcionService.getAllInscripciones();
+    public ResponseEntity<List<InscripcionDTO>> getAllInscripciones() {
+        return ResponseEntity.ok(inscripcionService.getAllInscripciones());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<InscripcionDTO> getInscripcionById(@PathVariable Long id) {
-        InscripcionDTO inscripcion = inscripcionService.getInscripcionById(id);
-        return ResponseEntity.ok(inscripcion);
+        Optional<InscripcionDTO> inscripcionDTO = inscripcionService.getInscripcionById(id);
+        return inscripcionDTO.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<InscripcionDTO> createInscripcion(@RequestBody InscripcionDTO inscripcionDTO) {
         InscripcionDTO createdInscripcion = inscripcionService.createInscripcion(inscripcionDTO);
-        return ResponseEntity.ok(createdInscripcion);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdInscripcion);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<InscripcionDTO> updateInscripcion(@PathVariable Long id, @RequestBody InscripcionDTO inscripcionDTO) {
         InscripcionDTO updatedInscripcion = inscripcionService.updateInscripcion(id, inscripcionDTO);
-        return ResponseEntity.ok(updatedInscripcion);
+        return updatedInscripcion != null ? ResponseEntity.ok(updatedInscripcion) : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
