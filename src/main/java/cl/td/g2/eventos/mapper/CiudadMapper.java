e4adb3b06339169320f34364e07633317fe99921
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-08-01 00:06:26 -0400

    Se añadió la conversión de listas de entidades Ciudad a listas de DTOs de Ciudad en CiudadMapper.

--- src/main/java/cl/td/g2/eventos/mapper/CiudadMapper.java
+++ src/main/java/cl/td/g2/eventos/mapper/CiudadMapper.java
@@ -2,10 +2,14 @@ package cl.td.g2.eventos.mapper;
 
 import cl.td.g2.eventos.dto.CiudadDTO;
 import cl.td.g2.eventos.model.Ciudad;
+
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CiudadMapper {
     CiudadDTO toDTO(Ciudad ciudad);
     Ciudad toEntity(CiudadDTO ciudadDTO);
+	List<CiudadDTO> toDTO(List<Ciudad> ciudades);
 }
