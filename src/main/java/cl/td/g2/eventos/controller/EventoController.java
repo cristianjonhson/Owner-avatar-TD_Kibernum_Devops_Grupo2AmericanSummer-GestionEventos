53323a17a125d28461f1b8348a4ade87d788c83b
Martin Machmar <mmf279@gmail.com>
2024-08-15 16:49:02 -0400

    implementacion de swagger

--- src/main/java/cl/td/g2/eventos/controller/EventoController.java
+++ src/main/java/cl/td/g2/eventos/controller/EventoController.java
@@ -2,6 +2,8 @@ package cl.td.g2.eventos.controller;
 
 import cl.td.g2.eventos.dto.EventoDTO;
 import cl.td.g2.eventos.service.EventoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,35 +15,41 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/eventos")
+@Tag(name="Servicios de Evento")
 public class EventoController {
 
     @Autowired
     private EventoService eventoService;
 
     @GetMapping
+    @Operation(summary="Obtener todos los Eventos")
     public ResponseEntity<List<EventoDTO>> getAllEventos() {
         return ResponseEntity.ok(eventoService.getAllEventos());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary="Obtener un Evento")
     public ResponseEntity<EventoDTO> getEventoById(@PathVariable Long id) {
         EventoDTO eventoDTO = eventoService.getEventoById(id);
         return ResponseEntity.ok(eventoDTO);
     }
 
     @PostMapping
+    @Operation(summary="Crear un Evento")
     public ResponseEntity<EventoDTO> createEvento(@Valid @RequestBody EventoDTO eventoDTO) {
         EventoDTO createdEvento = eventoService.createEvento(eventoDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdEvento);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary="Actualizar un Evento")
     public ResponseEntity<EventoDTO> updateEvento(@Valid @PathVariable Long id, @RequestBody EventoDTO eventoDTO) {
         EventoDTO updatedEvento = eventoService.updateEvento(id, eventoDTO);
         return ResponseEntity.ok(updatedEvento);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary="Eliminar un Evento")
     public ResponseEntity<Void> deleteEvento(@PathVariable Long id) {
         eventoService.deleteEvento(id);
         return ResponseEntity.noContent().build();
