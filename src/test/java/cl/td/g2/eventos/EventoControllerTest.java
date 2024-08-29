76ee5d6bb1f9a3ccb9153b61ae7e85f231860067
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-08-28 21:10:21 -0400

    Creacion prueba unitaria del Controller Usuario

--- src/test/java/cl/td/g2/eventos/EventoControllerTest.java
+++ src/test/java/cl/td/g2/eventos/EventoControllerTest.java
@@ -10,7 +10,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
