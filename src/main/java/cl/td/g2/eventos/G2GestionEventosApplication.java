78253c7eadf60e53df6ceadc1c84481e2b211646
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-10-09 22:23:34 -0300

    fix: limpieza de codigo

--- src/main/java/cl/td/g2/eventos/G2GestionEventosApplication.java
+++ src/main/java/cl/td/g2/eventos/G2GestionEventosApplication.java
@@ -2,7 +2,6 @@ package cl.td.g2.eventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication//(exclude = SecurityAutoConfiguration.class)
@@ -12,5 +11,4 @@ public class G2GestionEventosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(G2GestionEventosApplication.class, args);
 	}
-
 }
