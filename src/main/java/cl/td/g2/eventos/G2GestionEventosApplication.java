efc139dbf8deea6ff4f437a32481764ba256de80
Martin Machmar <mmf279@gmail.com>
2024-10-12 23:26:19 -0300

    prueba webhook

--- src/main/java/cl/td/g2/eventos/G2GestionEventosApplication.java
+++ src/main/java/cl/td/g2/eventos/G2GestionEventosApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class G2GestionEventosApplication {
 
 	public static void main(String[] args) {
-		System.out.println("prueba webhook");
 		SpringApplication.run(G2GestionEventosApplication.class, args);
 	}
 }
